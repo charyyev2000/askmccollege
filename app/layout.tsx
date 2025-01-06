a2315eab51e6548d69274b0ff179e3e5ec1fe985
Myrat Charyyev <charyyev2110@gmail.com>
2025-01-06 00:50:58 -0600

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import GoogleAnalytics from "@/scripts/google_analytics";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+import "dotenv/config";
+
+const { GA_PUBLIC_MEASUREMENT_ID } = process.env;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,7 +50,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <GoogleAnalytics />
+      {/* <GoogleAnalytics /> */}
+      <GoogleAnalytics gaId={GA_PUBLIC_MEASUREMENT_ID as string} />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
