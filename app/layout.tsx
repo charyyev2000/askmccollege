d8efab93792a450240bc7d6e6e90ffa8633dd7c3
Myrat Charyyev <charyyev2110@gmail.com>
2025-01-06 00:18:33 -0600

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "@/scripts/google_analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,6 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <GoogleAnalytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ scripts/google_analytics.tsx
@@ -0,0 +1,37 @@
+import Script from "next/script";
+
+import "dotenv/config";
+
+const { GA_PUBLIC_MEASUREMENT_ID } = process.env;
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id${GA_PUBLIC_MEASUREMENT_ID}`}
+      />
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_PUBLIC_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-GBB2XL2MBW"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-GBB2XL2MBW');
+// </script>
